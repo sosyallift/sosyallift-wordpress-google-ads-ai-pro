98679261e0e2515c8d080c0b045c6c5ce9f070c6
sosyallift <sosyallift@gmail.com>
2026-01-21 08:34:52 +0300

    Add files via upload

---
+++ vendor/autoload.php
@@ -0,0 +1,20 @@
+﻿@'
+<?php
+// Manuel autoloader for SosyalliftAIPro namespace
+spl_autoload_register(function ($class) {
+    $prefix = 'SosyalliftAIPro\\';
+    $base_dir = __DIR__ . '/../';
+    
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+'@ | Out-File -FilePath "vendor\autoload.php" -Encoding UTF8
