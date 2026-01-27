59ba8e96f263252263a65616f42e0de251d5c984
sosyallift <sosyallift@gmail.com>
2026-01-27 23:13:44 +0300

    Delete Includes directory

--- Includes/interfaces/ScorerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace SosyalliftAIPro\Includes\Interfaces;
-
-interface ScorerInterface {
-    public function calculate(array $data): float;
-    public function get_score_range(): array;
-    public function get_factors(): array;
-    public function explain_score(float $score): string;
-}
